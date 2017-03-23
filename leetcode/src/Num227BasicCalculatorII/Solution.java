cdfde414de38da59b71c4c13000e5a8080824f1c
silence <1499191187@qq.com>
2017-03-23 20:59:43 +0800

    add annotations

--- leetcode/src/Num227BasicCalculatorII/Solution.java
+++ leetcode/src/Num227BasicCalculatorII/Solution.java
@@ -14,18 +14,22 @@ public class Solution {
         LinkedList<Integer> digitList = new LinkedList<Integer>();
         LinkedList<Character> operatorList = new LinkedList<Character>();
 
+        //注意这里的regex，如果写成"[+-*/]"就是错误的
+        //根据操作分裂字符串，将字符串分为数字digits和操作符operator
         String[] digits = s.split("[+-/*]");
         for(int i=0; i<s.length(); i++) {
             if(operatorSet.contains(s.charAt(i))) {
                 operatorList.add(s.charAt(i));
             }
         }
+        //数字去空格
         for(String digit : digits) {
             digitList.add(Integer.valueOf(digit.trim()));
         }
         int len = operatorList.size();
         char c = ' ';
         int x=0, y=0;
+        //先完成*/的运算
         while(len-- > 0) {
             c = operatorList.removeFirst();
             x = digitList.removeFirst();
@@ -39,6 +43,7 @@ public class Solution {
         }
         digitList.add(digitList.removeFirst());
 
+        //完成+-的运算，从先往后运算
         while(!operatorList.isEmpty()) {
             c = operatorList.removeFirst();
             x = digitList.removeFirst();
