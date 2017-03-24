0aa44d8c4146a002edea8c4b48db4133300ea989
zzsilence <1536918768@qq.com>
2017-03-24 16:54:12 +0800

    add some tests

--- classicalgorithms/src/sort/Test.java
+++ classicalgorithms/src/sort/Test.java
@@ -10,19 +10,25 @@ package sort;
 public class Test {
     public static void main(String[] args) {
         Double[] datas = new Double[1000];
+        Integer[] sameDatas = new Integer[1000];
         for(int i=0; i<datas.length; i++) {
             datas[i] = 10000 * Math.random();
+            if (Math.random() > 0.5) {
+                sameDatas[i] = 100;
+            }else {
+                sameDatas[i] = i;
+            }
         }
 
         System.out.println(System.currentTimeMillis());
-        QuickSort.sort(datas);
+        QuickSort.sort(sameDatas);
         System.out.println(System.currentTimeMillis());
-        for(int i=0; i<datas.length; i++) {
-            if(i>0 && datas[i]<datas[i-1]) {
+        for(int i=0; i<sameDatas.length; i++) {
+            if(i>0 && sameDatas[i]<sameDatas[i-1]) {
                 System.out.println("error");
                 break;
             }
-            System.out.print(datas[i] + " ");
+            System.out.print(sameDatas[i] + " ");
         }
     }
 }
