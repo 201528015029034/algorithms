53c3d86a755054dd55ef1f9f0ac028295d5f4a8e
zzsilence <1536918768@qq.com>
2017-03-24 13:57:50 +0800

    test sort

---
+++ classicalgorithms/src/sort/Test.java
@@ -0,0 +1,28 @@
+package sort;
+
+/**
+ * Created by test on 2017/3/24.
+ */
+
+/**
+ * 测试排序
+ */
+public class Test {
+    public static void main(String[] args) {
+        Double[] datas = new Double[1000];
+        for(int i=0; i<datas.length; i++) {
+            datas[i] = 10000 * Math.random();
+        }
+
+        System.out.println(System.currentTimeMillis());
+        QuickSort.sort(datas);
+        System.out.println(System.currentTimeMillis());
+        for(int i=0; i<datas.length; i++) {
+            if(i>0 && datas[i]<datas[i-1]) {
+                System.out.println("error");
+                break;
+            }
+            System.out.print(datas[i] + " ");
+        }
+    }
+}
