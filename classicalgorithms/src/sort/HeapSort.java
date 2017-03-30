d76e643bd33e522e0e40ddf761da1354a4fe79fa
silence <1499191187@qq.com>
2017-03-31 00:08:50 +0800

    change some wrong annotations

--- classicalgorithms/src/sort/HeapSort.java
+++ classicalgorithms/src/sort/HeapSort.java
@@ -8,11 +8,11 @@ package sort;
 public class HeapSort {
     public static void sort(int[] nums) {
         int N = nums.length;
-        //建堆操作
+        //建堆操作，大根堆，堆索引是从0开始的
         for(int i=N/2; i>=0; i--) {
             sink(nums, i, N);
         }
-        //堆排序
+        //堆排序，堆索引是从0开始的
         while(N > 1) {
             exch(nums, 0, --N);
             sink(nums, 0, N);
@@ -26,9 +26,13 @@ public class HeapSort {
      * @param N 下沉操作的尾边界，不包含N
      */
     private static void sink(int[] nums, int i, int N) {
-        //i*2+1代表它的右子节点的索引，保证小于尾边界N
+        //节点索引是从0开始的，i*2+1代表它的左子节点的索引，保证小于尾边界N
+        //      0
+        //     / \
+        //    1  2
         while(i*2+1 < N) {
-            int k = 2*i+1;
+            int k = i*2+1;
+            //如果右子节点存在，且大于左子节点
             if(k < N-1 && nums[k+1] > nums[k]) k++;
             if(nums[k] < nums[i]) break;
             exch(nums, i, k);
